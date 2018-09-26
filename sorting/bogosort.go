420a61965991e778bf05218632b7111616c16908
Matteo Madeddu <matteo.madeddu@gmail.com>
2018-09-26 22:48:43 +0200

    Add sorting package

--- sorting/bogosort.go
+++ sorting/bogosort.go
@@ -1,17 +1,14 @@
 package bogosort
 
 import (
-	"fmt"
 	"github.com/made2591/go-algorithms/sorting/common"
 )
 
 func Bogosorting(vals []int) (a []int) {
 
-	for s := common.Sorted(vals) {
+	for common.Sorted(vals) {
 		common.Shuffle(vals)
-		s = common.Sorted(vals)
 	}
-
-	return a
+	return vals
 
 }
