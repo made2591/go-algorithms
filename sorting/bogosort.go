cd14873c784747884e4c6ec1c7bd709bf7b16851
Matteo Madeddu <matteo.madeddu@gmail.com>
2018-09-26 23:08:14 +0200

    Add testing of Bogosorting

--- sorting/bogosort.go
+++ sorting/bogosort.go
@@ -1,6 +1,7 @@
 package bogosort
 
 import (
+	"testing"
 	"github.com/made2591/go-algorithms/sorting/common"
 )
 
@@ -11,4 +12,11 @@ func Bogosorting(vals []int) (a []int) {
 	}
 	return vals
 
+}
+
+func TestBogosorting(t *testing.T) {
+	ordered := Bogosorting([]int{3, 2, 1})
+	if common.Equal(ordered, []int{3, 2, 1}) {
+		t.Errorf("Bogosorting was incorrect, got: %d, want: %d.", ordered, []int{3, 2, 1})
+	}
 }
--- sorting/common/util.go
+++ sorting/common/util.go
@@ -23,3 +23,15 @@ func Shuffle(vals []int) {
 		vals = vals[:n-1]
 	}
 }
+
+func Equal(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
